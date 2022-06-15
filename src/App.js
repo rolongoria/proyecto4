dc08b0fd871345fcf9c8d4b4619f49a0d75c1676
Rodrigo Longoria <ra.longoria86@gmail.com>
2022-06-15 15:54:37 -0500

    remove imports

--- src/App.js
+++ src/App.js
@@ -1,14 +1,7 @@
-import { Outlet, Link } from "react-router-dom";
+import { Outlet } from "react-router-dom";
 import './App.css';
 import Header from './components/Header';
-import Home from './routes/Home'
-import PicturesCarousel from './components/PicturesCarousel';
-import ReserveForm from './components/ReserveForm';
 
-import Pupusas from './images/pupusas.png';
-import Flautas from './images/flautas.png';
-import Enchiladas from './images/enchiladas.png';
-import Gorditas from './images/gorditas.png';
 import Footer from "./components/Footer";
 
 function App() {
@@ -17,9 +10,7 @@ function App() {
       <Header />
       <Outlet />
       <Footer />
-
       
-
     </div>
   );
 }
